b10f2605d21b1fef94f2ed14e631db458bfb319d
Anton <antonwelliver27@gmail.com>
2020-02-03 12:24:14 +0100

    fixed cancelbtn bug

--- js/main.js
+++ js/main.js
@@ -2,17 +2,21 @@
 function addNewGeofence() {
     var createGeofenceView = document.getElementById("createGeofenceView");
     var manageGeofenceTable = document.getElementById("manageGeofenceTable");
+    var statusDataHeader = document.getElementById("statusdataheader");
     if (createGeofenceView.style.display !== "grid") {
         createGeofenceView.style.display = "grid";
         manageGeofenceTable.style.display = "none";
+        statusDataHeader.style.display = "none";
     }
 }
 
 function cancelManageView() {
     var createGeofenceView = document.getElementById("createGeofenceView");
     var manageGeofenceTable = document.getElementById("manageGeofenceTable");
+    var statusDataHeader = document.getElementById("statusdataheader");
     if (manageGeofenceTable.style.display === "none") {
         manageGeofenceTable.style.display = "table";
         createGeofenceView.style.display = "none";
+        statusDataHeader.style.display = "grid";
     }
 }
