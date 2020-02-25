29a67c0e32b2f5464e08cbbad43899e0c21e31d0
Anton <antonwelliver27@gmail.com>
2020-02-25 11:12:08 +0100

    added modal for map in wlan.html and added map

---
+++ js/map.js
@@ -0,0 +1,11 @@
+//Function mapbox
+mapboxgl.accessToken = 'pk.eyJ1IjoibGFyc24iLCJhIjoiY2s2YzByNTh4MDZjdTNxb21lMjY3NjBnMSJ9.bbEbVqLCn7Oco1FXsI1nFQ'; // token key
+
+//set out the map
+var map = new mapboxgl.Map({
+    container: 'map', // container id
+    style: 'mapbox://styles/mapbox/streets-v11', //hosted style id
+    center: [11.975482095909456, 57.69282011876044], // starting position
+    zoom: 17, // starting zoom
+
+});
