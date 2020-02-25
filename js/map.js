fdaf7d6c47d32fd1b5a6c1a8b3648b580a5470a2
Anton <antonwelliver27@gmail.com>
2020-02-25 12:05:50 +0100

    map styling and position

--- js/map.js
+++ js/map.js
@@ -6,6 +6,23 @@ var map = new mapboxgl.Map({
     container: 'map', // container id
     style: 'mapbox://styles/mapbox/streets-v11', //hosted style id
     center: [11.975482095909456, 57.69282011876044], // starting position
-    zoom: 17, // starting zoom
+    zoom: 16, // starting zoom
 
+});
+
+var biggerSmaller = '';
+
+map.on('load', function () {
+    var mapCanvas = document.getElementsByClassName('mapboxgl-canvas')[0];
+    var mapDiv = document.getElementById('map');
+    var mapButton = document.getElementById('mapbtn');
+
+    mapButton.onclick = function () {
+        if (biggerSmaller !== 'smaller') {
+            mapDiv.style.width = '100%';
+            mapCanvas.style.width = '100%';
+            biggerSmaller = 'smaller';
+        }
+        map.resize();
+    };
 });
