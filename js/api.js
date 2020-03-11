24cfa546dd6c111c51fdb6627b8a30e892909900
Lars Nihlmark <larsnihlmark@gmail.com>
2020-03-11 15:14:51 +0100

    delete button to remove ID-API

--- js/api.js
+++ js/api.js
@@ -1,38 +1,20 @@
-
 const apiGeofence = "https://api.proximi.fi/core/geofences";
 const apiFloor = "https://api.proximi.fi/core/floors";
 const apiPlace = "https://api.proximi.fi/core/places";
 const apiDepartments = "https://api.proximi.fi/core/departments";
 const AuthStr = 'Bearer '.concat("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImlzcyI6ImM4OTA0YzkyLTlmN2MtNGE3Yy1iZDZjLTZiMjBiMTczZDEwZSIsInR5cGUiOiJ1c2VyIiwidXNlciI6IlZlZGVmb3JzIEVtYW51ZWwiLCJ1c2VyX2lkIjoiN2VmNTI2MzctZGFmNy00ZGRlLTljMjAtNGIwNmZhMjJhNTIyIiwidGVuYW50X2lkIjoiYzg5MDRjOTItOWY3Yy00YTdjLWJkNmMtNmIyMGIxNzNkMTBlIn0.mYpZ--Ecuzc37FHCh4oyGj1gdy_CEpvOhQg0vTS0alE");
 
-
-
-/* var tableRow = document.querySelector(".getRow");
-var dict = {"one" : [15, 4.5],
-        "two" : [34, 3.3],
-        "three" : [67, 5.0],
-        "four" : [32, 4.1]};
-
-        var dictstring = JSON.stringify(dict); */
-
-
 //Geofence API
-function axiosTest(newArray) {
-
+function axiosTest(newArray){
   var newArray = [];
 
-  axios.get(apiGeofence, { headers: { Authorization: AuthStr } })
-    .then(function (response) {
-      for (var i = 0; i < response.data.length; i++) {
+  axios.get(apiGeofence,{ headers: { Authorization: AuthStr } })
+    .then(function(response){
+      for (var i = 0; i < response.data.length; i++){
         newArray[i] = (response.data[i]);
-
-      }
-
-      /* console.log(newArray); */
-      /* console.log(response); */
-
+      } 
+      
       function generateTable(table, data) {
-        console.log(data);
         for (element of data) {
           var row = table.insertRow();
           var name = element.name;
@@ -44,8 +26,8 @@ function axiosTest(newArray) {
           var latitud = element.area.lat;
           var longitud = element.area.lng;
           var createdAt = element.createdAt;
-
-          var sliceDate = createdAt.slice(0, 10);
+          var getID  = element.id;
+          var sliceDate = createdAt.slice(0, 10 );
 
           var cell = row.insertCell(0);
           var cell1 = row.insertCell(1);
@@ -57,7 +39,7 @@ function axiosTest(newArray) {
           var cell7 = row.insertCell(7);
           var cell8 = row.insertCell(8);
           var cell9 = row.insertCell(9);
-
+         
           var nameValue = document.createTextNode(name);
           var typeValue = document.createTextNode(type);
           var addressValue = document.createTextNode(address);
@@ -67,40 +49,48 @@ function axiosTest(newArray) {
           var latitudValue = document.createTextNode(latitud);
           var longitudValue = document.createTextNode(longitud);
           var createdAtValue = document.createTextNode(sliceDate);
-
+          
 
           if (element.place_name == null) {
-            placeValue = document.createTextNode("");
+            placeValue = document.createTextNode("");  
           }
-          if (element.floor_name == null) {
+          if(element.floor_name == null){
             floorValue = document.createTextNode("");
           }
-          if (element.department_name == null) {
+          if(element.department_name == null){
             departmentValue = document.createTextNode("");
           }
 
-          function createEditButton() {
+          function createEditButton(){
             var editButton = document.createElement('a');
-
             editButton.href = 'editGeofence.html';
             editButton.className = 'editIconButton, far fa-edit';
             editButton.id = 'editButton';
-            editButton.style = "margin-left: 7px; margin-right: 20px; color: rgb(20, 134, 168);"
-
+            editButton.style = "margin-left: 7px; margin-right: 20px; color: rgb(20, 134, 168);"  
+          
             return editButton;
           }
-          function createDeleteButton() {
+          function createDeleteButton(){
             var deleteButton = document.createElement('a');
-
-            deleteButton.href = '#';
             deleteButton.className = 'deleteIconButton, far fa-trash-alt';
-            deleteButton.id = 'deleteButton';
-            deleteButton.style = "color: rgb(248, 97, 97);"
-
+            deleteButton.id = getID;
+            deleteButton.style = "color: rgb(248, 97, 97);" 
+            deleteButton.onclick = function(){
+              var apiGeofenceDelete = "https://api.proximi.fi/core/geofences/"; 
+              axios.delete(apiGeofenceDelete + deleteButton.id, {
+                  headers: {
+                    Authorization: AuthStr
+                  }
+                })
+                .then(function(response){
+                  
+                }) .catch(function(error){
+                  console.log(error)
+                })
+                setTimeout(function(){ window.location.reload(); }, 300);
+              }
             return deleteButton;
           }
-
-
           cell.appendChild(nameValue);
           cell1.appendChild(typeValue);
           cell2.appendChild(addressValue);
@@ -112,85 +102,47 @@ function axiosTest(newArray) {
           cell8.appendChild(createdAtValue);
           cell9.appendChild(createEditButton());
           cell9.appendChild(createDeleteButton());
-
         }
-
       }
-
       let table = document.querySelector(".center");
       generateTable(table, newArray);
-
     })
-    .catch(function (error) {
-
+    .catch(function(error){
       console.log(error);
-
-
-    })
-
+    })   
 }
 axiosTest();
 
-
-
-
 /* // Floor API
- axios.get(apiFloor,{ headers: { Authorization: AuthStr } })
-  .then(function(response){
-    console.log(response.data)
-    geofenceFloor.innerHTML = response.data[1].name;
-
-  })
-  .catch(function(error){
-    geofenceName.innerHTML = ("error")
-
-  })
+axios.get(apiFloor,{ headers: { Authorization: AuthStr } })
+.then(function(response){
+  console.log(response.data)
+  geofenceFloor.innerHTML = response.data[1].name;
+  
+})
+.catch(function(error){
+  geofenceName.innerHTML = ("error")
+  
+})
 //Place API
- axios.get(apiPlace,{ headers: { Authorization: AuthStr } })
-  .then(function(response){
-    console.log(response.data)
-    geofencePlace.innerHTML = response.data[2].name;
-
-  })
-  .catch(function(error){
-    geofenceName.innerHTML = ("error")
-
-  })
-  //Departments API
- axios.get(apiDepartments,{ headers: { Authorization: AuthStr } })
-  .then(function(response){
-    console.log(response.data)
-    geofenceDepartment.innerHTML = response.data[7].name;
-
-  })
-  .catch(function(error){
-    geofenceName.innerHTML = ("error")
-
-  }) */
-
-/* function generateTableHead(table, data) {
-  let thead = table.createTHead();
-  let row = thead.insertRow();
-  for (let key of data) {
-    let th = document.createElement("th");
-    let text = document.createTextNode(key);
-    th.appendChild(text);
-    row.appendChild(th);
-  }
-} */
-
-/*   function generateTable(table, data) {
-    for (let element of data) {
-      let row = table.insertRow();
-      for (key in element) {
-        let cell = row.insertCell();
-        let text = document.createTextNode(element[key]);
-        cell.appendChild(text);
-      }
-    }
-  }
-
-  let table = document.querySelector("table");
-  let data = Object.keys(mountains[0]);
-  generateTable(table, mountains); */
-/* generateTableHead(table, data); */
+axios.get(apiPlace,{ headers: { Authorization: AuthStr } })
+.then(function(response){
+  console.log(response.data)
+  geofencePlace.innerHTML = response.data[2].name;
+  
+})
+.catch(function(error){
+  geofenceName.innerHTML = ("error")
+  
+})
+//Departments API
+axios.get(apiDepartments,{ headers: { Authorization: AuthStr } })
+.then(function(response){
+  console.log(response.data)
+  geofenceDepartment.innerHTML = response.data[7].name;
+  
+})
+.catch(function(error){
+  geofenceName.innerHTML = ("error")
+  
+}) */
